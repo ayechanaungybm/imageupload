d849c1fd842d7afcaab8581499f639b03c4ec87f
Aye Chan Aung <76277729+studentinucsy@users.noreply.github.com>
2023-03-12 16:40:05 +0630

    Update HelloWorld.java

--- imageupload/src/main/java/com/example/imageupload/HelloWorld.java
+++ imageupload/src/main/java/com/example/imageupload/HelloWorld.java
@@ -3,7 +3,7 @@ package com.example.imageupload;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("How are you");
 
 	}
 
