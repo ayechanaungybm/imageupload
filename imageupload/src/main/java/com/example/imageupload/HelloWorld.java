a46f46d476020187c5cd6cc4809f685b8f4d0848
Aye Chan Aung <76277729+studentinucsy@users.noreply.github.com>
2023-03-12 16:45:56 +0630

    Update HelloWorld.java

--- imageupload/src/main/java/com/example/imageupload/HelloWorld.java
+++ imageupload/src/main/java/com/example/imageupload/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("How are you");
+		System.out.pringln("Hi This is the code written by developer A.");
 
 	}
 
